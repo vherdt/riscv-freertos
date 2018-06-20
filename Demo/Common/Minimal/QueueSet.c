fd0073e4f131d4382251224172faa8a29f40b188
Vladimir Herdt <vherdt@informatik.uni-bremen.de>
2018-06-20 15:20:33 +0200

    update port to FreeRTOSv10.0.1 and added FreeRTOS-Plus-UDP component

--- Demo/Common/Minimal/QueueSet.c
+++ Demo/Common/Minimal/QueueSet.c
@@ -1,71 +1,29 @@
 /*
-    FreeRTOS V8.2.3 - Copyright (C) 2015 Real Time Engineers Ltd.
-    All rights reserved
-
-    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
-
-    This file is part of the FreeRTOS distribution.
-
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-
-    ***************************************************************************
-    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
-    >>!   distribute a combined work that includes FreeRTOS without being   !<<
-    >>!   obliged to provide the source code for proprietary components     !<<
-    >>!   outside of the FreeRTOS kernel.                                   !<<
-    ***************************************************************************
-
-    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
-    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-    FOR A PARTICULAR PURPOSE.  Full license text is available on the following
-    link: http://www.freertos.org/a00114.html
-
-    ***************************************************************************
-     *                                                                       *
-     *    FreeRTOS provides completely free yet professionally developed,    *
-     *    robust, strictly quality controlled, supported, and cross          *
-     *    platform software that is more than just the market leader, it     *
-     *    is the industry's de facto standard.                               *
-     *                                                                       *
-     *    Help yourself get started quickly while simultaneously helping     *
-     *    to support the FreeRTOS project by purchasing a FreeRTOS           *
-     *    tutorial book, reference manual, or both:                          *
-     *    http://www.FreeRTOS.org/Documentation                              *
-     *                                                                       *
-    ***************************************************************************
-
-    http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
-    the FAQ page "My application does not run, what could be wrong?".  Have you
-    defined configASSERT()?
-
-    http://www.FreeRTOS.org/support - In return for receiving this top quality
-    embedded software for free we request you assist our global community by
-    participating in the support forum.
-
-    http://www.FreeRTOS.org/training - Investing in training allows your team to
-    be as productive as possible as early as possible.  Now you can receive
-    FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
-    Ltd, and the world's leading authority on the world's leading RTOS.
-
-    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
-    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
-    compatible FAT file system, and our tiny thread aware UDP/IP stack.
-
-    http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
-    Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
-
-    http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
-    Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
-    licenses offer ticketed support, indemnification and commercial middleware.
-
-    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
-    engineered and independently SIL3 certified version for use in safety and
-    mission critical applications that require provable dependability.
-
-    1 tab == 4 spaces!
-*/
+ * FreeRTOS Kernel V10.0.1
+ * Copyright (C) 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://www.FreeRTOS.org
+ * http://aws.amazon.com/freertos
+ *
+ * 1 tab == 4 spaces!
+ */
 
 /*
  * Tests the use of queue sets.
@@ -122,7 +80,7 @@ queuesetPRIORITY_CHANGE_LOOPS number of values are sent to a queue. */
 /* A delay inserted when the Tx task changes its priority to be above the idle
 task priority to ensure the idle priority tasks get some CPU time before the
 next iteration of the queue set Tx task. */
-#define queuesetTX_LOOP_DELAY	( 200 / portTICK_PERIOD_MS )
+#define queuesetTX_LOOP_DELAY	pdMS_TO_TICKS( ( TickType_t ) 200 )
 
 /* The allowable maximum deviation between a received value and the expected
 received value.  A deviation will occur when data is received from a queue
@@ -187,8 +145,8 @@ static void prvChangeRelativePriorities( void );
  * Local pseudo random number seed and return functions.  Used to avoid calls
  * to the standard library.
  */
-static uint32_t prvRand( void );
-static void prvSRand( uint32_t ulSeed );
+static size_t prvRand( void );
+static void prvSRand( size_t uxSeed );
 
 /*-----------------------------------------------------------*/
 
@@ -223,7 +181,7 @@ expected. */
 static volatile uint32_t ulISRTxValue = queuesetINITIAL_ISR_TX_VALUE;
 
 /* Used by the pseudo random number generator. */
-static uint32_t ulNextRand = 0;
+static size_t uxNextRand = 0;
 
 /* The task handles are stored so their priorities can be changed. */
 TaskHandle_t xQueueSetSendingTask, xQueueSetReceivingTask;
@@ -234,14 +192,18 @@ void vStartQueueSetTasks( void )
 {
 	/* Create the tasks. */
 	xTaskCreate( prvQueueSetSendingTask, "SetTx", configMINIMAL_STACK_SIZE, NULL, queuesetMEDIUM_PRIORITY, &xQueueSetSendingTask );
-	xTaskCreate( prvQueueSetReceivingTask, "SetRx", configMINIMAL_STACK_SIZE, ( void * ) xQueueSetSendingTask, queuesetMEDIUM_PRIORITY, &xQueueSetReceivingTask );
 
-	/* It is important that the sending task does not attempt to write to a
-	queue before the queue has been created.  It is therefore placed into the
-	suspended state before the scheduler has started.  It is resumed by the
-	receiving task after the receiving task has created the queues and added the
-	queues to the queue set. */
-	vTaskSuspend( xQueueSetSendingTask );
+	if( xQueueSetSendingTask != NULL )
+	{
+		xTaskCreate( prvQueueSetReceivingTask, "SetRx", configMINIMAL_STACK_SIZE, ( void * ) xQueueSetSendingTask, queuesetMEDIUM_PRIORITY, &xQueueSetReceivingTask );
+
+		/* It is important that the sending task does not attempt to write to a
+		queue before the queue has been created.  It is therefore placed into
+		the suspended state before the scheduler has started.  It is resumed by
+		the receiving task after the receiving task has created the queues and
+		added the queues to the queue set. */
+		vTaskSuspend( xQueueSetSendingTask );
+	}
 }
 /*-----------------------------------------------------------*/
 
@@ -295,24 +257,25 @@ BaseType_t xReturn = pdPASS, x;
 
 static void prvQueueSetSendingTask( void *pvParameters )
 {
-uint32_t ulTaskTxValue = 0, ulQueueToWriteTo;
+uint32_t ulTaskTxValue = 0;
+size_t uxQueueToWriteTo;
 QueueHandle_t xQueueInUse;
 
 	/* Remove compiler warning about the unused parameter. */
 	( void ) pvParameters;
 
 	/* Seed mini pseudo random number generator. */
-	prvSRand( ( uint32_t ) &ulTaskTxValue );
+	prvSRand( ( size_t ) &ulTaskTxValue );
 
 	for( ;; )
 	{
 		/* Generate the index for the queue to which a value is to be sent. */
-		ulQueueToWriteTo = prvRand() % queuesetNUM_QUEUES_IN_SET;
-		xQueueInUse = xQueues[ ulQueueToWriteTo ];
+		uxQueueToWriteTo = prvRand() % queuesetNUM_QUEUES_IN_SET;
+		xQueueInUse = xQueues[ uxQueueToWriteTo ];
 
 		/* Note which index is being written to to ensure all the queues are
 		used. */
-		( ulQueueUsedCounter[ ulQueueToWriteTo ] )++;
+		( ulQueueUsedCounter[ uxQueueToWriteTo ] )++;
 
 		/* Send to the queue to unblock the task that is waiting for data to
 		arrive on a queue within the queue set to which this queue belongs. */
@@ -419,7 +382,7 @@ TickType_t xBlockTime;
 		}
 
 		/* Wait for a message to arrive on one of the queues in the set. */
-		xActivatedQueue = xQueueSelectFromSet( xQueueSet, portMAX_DELAY );		
+		xActivatedQueue = xQueueSelectFromSet( xQueueSet, portMAX_DELAY );
 
 		if( xActivatedQueue == NULL )
 		{
@@ -614,8 +577,9 @@ uint32_t ulReceived;
 static void prvSendToQueueInSetFromISR( void )
 {
 static BaseType_t xQueueToWriteTo = 0;
+uint32_t ulTxValueSnapshot = ulISRTxValue;
 
-	if( xQueueSendFromISR( xQueues[ xQueueToWriteTo ], ( void * ) &ulISRTxValue, NULL ) == pdPASS )
+	if( xQueueSendFromISR( xQueues[ xQueueToWriteTo ], ( void * ) &ulTxValueSnapshot, NULL ) == pdPASS )
 	{
 		ulISRTxValue++;
 
@@ -719,15 +683,15 @@ uint32_t ulValueToSend = 0;
 }
 /*-----------------------------------------------------------*/
 
-static uint32_t prvRand( void )
+static size_t prvRand( void )
 {
-	ulNextRand = ( ulNextRand * 1103515245UL ) + 12345UL;
-    return ( ulNextRand / 65536UL ) % 32768UL;
+	uxNextRand = ( uxNextRand * ( size_t ) 1103515245 ) + ( size_t ) 12345;
+	return ( uxNextRand / ( size_t ) 65536 ) % ( size_t ) 32768;
 }
 /*-----------------------------------------------------------*/
 
-static void prvSRand( uint32_t ulSeed )
+static void prvSRand( size_t uxSeed )
 {
-    ulNextRand = ulSeed;
+	uxNextRand = uxSeed;
 }
 
---
+++ Plus/FreeRTOS-Plus-UDP/portable/NetworkInterface/riscv-vp/NetworkInterface.c
@@ -0,0 +1,203 @@
+// based on the porting guide available from: https://www.freertos.org/FreeRTOS-Plus/FreeRTOS_Plus_UDP/Embedded_Ethernet_Porting.shtml#simple_porting_embedded_ethernet
+
+/* Standard includes. */
+#include <stdint.h>
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+/* FreeRTOS+UDP includes. */
+#include "FreeRTOS_UDP_IP.h"
+#include "FreeRTOS_IP_Private.h"
+#include "FreeRTOS_Sockets.h"
+#include "NetworkBufferManagement.h"
+
+/* Arch specific */
+#include "irq.h"
+
+/* Demo includes. */
+#include "NetworkInterface.h"
+
+/* If ipconfigETHERNET_DRIVER_FILTERS_FRAME_TYPES is set to 1, then the Ethernet
+driver will filter incoming packets and only pass the stack those packets it
+considers need processing.  In this case ipCONSIDER_FRAME_FOR_PROCESSING() can
+be #defined away.  If ipconfigETHERNET_DRIVER_FILTERS_FRAME_TYPES is set to 0
+then the Ethernet driver will pass all received packets to the stack, and the
+stack must do the filtering itself.  In this case ipCONSIDER_FRAME_FOR_PROCESSING
+needs to call eConsiderFrameForProcessing. */
+#if ipconfigETHERNET_DRIVER_FILTERS_FRAME_TYPES != 1
+	#define ipCONSIDER_FRAME_FOR_PROCESSING( pucEthernetBuffer ) eProcessBuffer
+#else
+	#define ipCONSIDER_FRAME_FOR_PROCESSING( pucEthernetBuffer ) eConsiderFrameForProcessing( ( pucEthernetBuffer ) )
+#endif
+
+/*-----------------------------------------------------------*/
+// VP/HW interface
+
+static volatile uint32_t * const ETHERNET_STATUS_REG_ADDR = (uint32_t * const)0x30000000;
+static volatile uint32_t * const ETHERNET_RECV_SIZE_REG_ADDR = (uint32_t * const)0x30000004;
+static volatile uint32_t * const ETHERNET_RECV_DST_REG_ADDR = (uint32_t * const)0x30000008;
+static volatile uint32_t * const ETHERNET_SEND_SRC_REG_ADDR = (uint32_t * const)0x3000000c;
+static volatile uint32_t * const ETHERNET_SEND_SIZE_REG_ADDR = (uint32_t * const)0x30000010;
+
+#define ETHERNET_OPERATION_RECV 1
+#define ETHERNET_OPERATION_SEND 2
+
+
+size_t ReceiveSize( void ) {
+    return *ETHERNET_RECV_SIZE_REG_ADDR;
+}
+
+
+void ReceiveData( uint8_t *buf ) {
+    // assume that buf is large enough to hold ReceiveSize bytes
+    *ETHERNET_RECV_DST_REG_ADDR = (uint32_t)buf;
+    *ETHERNET_STATUS_REG_ADDR = ETHERNET_OPERATION_RECV;
+}
+
+
+void SendData( uint8_t *buf, size_t num_bytes ) {
+    *ETHERNET_SEND_SRC_REG_ADDR = (uint32_t)buf;
+    *ETHERNET_SEND_SIZE_REG_ADDR = (uint32_t)num_bytes;
+    *ETHERNET_STATUS_REG_ADDR = ETHERNET_OPERATION_SEND;
+}
+
+
+/*-----------------------------------------------------------*/
+// FreeRTOS interface
+
+SemaphoreHandle_t xEMACRxEventSemaphore;
+
+
+BaseType_t ethernet_mac_irq_handler( void ) {
+    BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
+    BaseType_t ans = xSemaphoreGiveFromISR(xEMACRxEventSemaphore, &pxHigherPriorityTaskWoken);
+
+	//portRETURN_FROM_ISR;
+	portYIELD_FROM_ISR( pxHigherPriorityTaskWoken );
+}
+
+
+/* The deferred interrupt handler is a standard RTOS task. */
+static void prvEMACDeferredInterruptHandlerTask( void *pvParameters )
+{
+    xNetworkBufferDescriptor_t *pxNetworkBuffer;
+    size_t xBytesReceived;
+    extern QueueHandle_t xNetworkEventQueue;
+    xIPStackEvent_t xRxEvent;
+
+    for( ;; )
+    {
+        /* Wait for the Ethernet MAC interrupt to indicate that another packet
+        has been received.  It is assumed xEMACRxEventSemaphore is a counting
+        semaphore (to count the Rx events) and that the semaphore has already
+        been created. */
+        xSemaphoreTake( xEMACRxEventSemaphore, portMAX_DELAY );
+
+        /* See how much data was received.  Here it is assumed ReceiveSize() is
+        a peripheral driver function that returns the number of bytes in the
+        received Ethernet frame. */
+        xBytesReceived = ReceiveSize();
+
+        if( xBytesReceived > 0 )
+        {
+            /* Allocate a network buffer descriptor that references an Ethernet
+            buffer large enough to hold the received data. */
+            pxNetworkBuffer = pxNetworkBufferGet( xBytesReceived, 0 );
+
+            if( pxNetworkBuffer != NULL )
+            {
+                /* pxNetworkBuffer->pucEthernetBuffer now points to an Ethernet
+                buffer large enough to hold the received data.  Copy the
+                received data into pcNetworkBuffer->pucEthernetBuffer.  Here it
+                is assumed ReceiveData() is a peripheral driver function that
+                copies the received data into a buffer passed in as the function's
+                parameter. */
+                ReceiveData( pxNetworkBuffer->pucEthernetBuffer );
+
+                /* See if the data contained in the received Ethernet frame needs
+                to be processed. */
+                if( eConsiderFrameForProcessing( pxNetworkBuffer->pucEthernetBuffer )
+                                                                      == eProcessBuffer )
+                {
+                    /* The event about to be sent to the IP stack is an Rx event. */
+                    xRxEvent.eEventType = eEthernetRxEvent;
+
+                    /* pvData is used to point to the network buffer descriptor that
+                    references the received data. */
+                    xRxEvent.pvData = ( void * ) pxNetworkBuffer;
+
+                    /* Send the data to the IP stack. */
+                    if( xQueueSendToBack( xNetworkEventQueue, &xRxEvent, 0 ) == pdFALSE )
+                    {
+                        /* The buffer could not be sent to the IP task so the buffer
+                        must be released. */
+                        vNetworkBufferRelease( pxNetworkBuffer );
+
+                        /* Make a call to the standard trace macro to log the
+                        occurrence. */
+                        iptraceETHERNET_RX_EVENT_LOST();
+                    }
+                    else
+                    {
+                        /* The message was successfully sent to the IP stack.  Call
+                        the standard trace macro to log the occurrence. */
+                        iptraceNETWORK_INTERFACE_RECEIVE();
+                    }
+                }
+                else
+                {
+                    /* The Ethernet frame can be dropped, but the Ethernet buffer
+                    must be released. */
+                    vNetworkBufferRelease( pxNetworkBuffer );
+                }
+            }
+            else
+            {
+                /* The event was lost because a network buffer was not available.
+                Call the standard trace macro to log the occurrence. */
+                iptraceETHERNET_RX_EVENT_LOST();
+            }
+        }
+    }
+}
+
+
+BaseType_t xNetworkInterfaceOutput( xNetworkBufferDescriptor_t * const pxNetworkBuffer )
+{
+    /* Simple network interfaces (as opposed to more efficient zero copy network
+    interfaces) just use Ethernet peripheral driver library functions to copy
+    data from the FreeRTOS+UDP buffer into the peripheral driver's own buffer.
+
+    This example assumes SendData() is a peripheral driver library function that
+    takes a pointer to the start of the data to be sent and the length of the
+    data to be sent as two separate parameters.  The start of the data is located
+    by pxNetworkBuffer->pucEthernetBuffer.  The length of the data is located
+    by pxNetworkBuffer->xDataLength. */
+    SendData( pxNetworkBuffer->pucEthernetBuffer, pxNetworkBuffer->xDataLength );
+
+    /* Call the standard trace macro to log the send event. */
+    iptraceNETWORK_INTERFACE_TRANSMIT();
+
+    /* It is assumed SendData() copies the data out of the FreeRTOS+UDP Ethernet
+    buffer.  The Ethernet buffer is therefore no longer needed, and must
+    be returned to the IP stack. */
+    vNetworkBufferRelease( pxNetworkBuffer );
+
+    return pdTRUE;
+}
+
+
+BaseType_t xNetworkInterfaceInitialise( void ) {
+    //TODO: install irq handler and setup receiving task
+    register_interrupt_handler(7, ethernet_mac_irq_handler);
+    
+    xEMACRxEventSemaphore = xSemaphoreCreateCounting(16, 0);
+    
+    xTaskCreate( prvEMACDeferredInterruptHandlerTask, "prvEMACDeferredInterruptHandlerTask", 1000, NULL, 3, NULL );
+    
+    return pdPASS;
+}
