54ead3a94e7b99cfeb970deb5861d8586b4a9b0a
Vladimir Herdt <vherdt@informatik.uni-bremen.de>
2018-07-05 09:47:52 +0200

    plus-udp updates

--- Demo/plus-udp/main.c
+++ Demo/plus-udp/main.c
@@ -1,9 +1,16 @@
-/* Kernel includes. */
+// based on https://www.freertos.org/FreeRTOS-Plus/FreeRTOS_Plus_UDP/FreeRTOS_UDP_IP_Embedded_Ethernet_Tutorial.shtml
+// and on https://www.freertos.org/FreeRTOS-Plus/FreeRTOS_Plus_UDP/API/vApplicationIPNetworkEventHook.shtml
+
+/* Kernel includes */
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
 #include "queue.h"
 #include "timers.h"
+
+/* UDP+Plus includes */
+#include "FreeRTOS_Sockets.h"
+#include "FreeRTOS_UDP_IP.h"
 
 /* RISCV includes */
 #include "arch/syscalls.h"
@@ -33,72 +40,224 @@ void vApplicationIdleHook( void );
  */
 void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
 
+/* Called by the IP stack when the network either connects or disconnects. */
+void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent );
+
+
+/*-----------------------------------------------------------*/
+/*
+ * Note: loopback interface seems not to be implemented in the FreeRTOS stack. You can either:
+ *
+ * 1) Filter packets during sending (in NetworkInterface.c) and manually inject them into the network queue (in case the IP address does match 127.0.0.1, and probably also modify the source and destination MAC addresses).
+ *
+ * 2) In the EthernetAdapter in the VP detect localhost packets and inject them back to the FreeRTOS application.
+ */
+
 /*-----------------------------------------------------------*/
 
 
-static volatile char * const TERMINAL_ADDR = (char * const)0x20000000;
-static volatile char * const SENSOR_INPUT_ADDR = (char * const)0x50000000;
-static volatile uint32_t * const SENSOR_SCALER_REG_ADDR = (uint32_t * const)0x50000080;
-static volatile uint32_t * const SENSOR_FILTER_REG_ADDR = (uint32_t * const)0x50000084;
+static void vSendUsingStandardInterface( void *pvParameters )
+{
+xSocket_t xSocket;
+struct freertos_sockaddr xDestinationAddress;
+uint8_t cString[ 50 ];
+uint32_t ulCount = 0UL;
+const TickType_t x1000ms = 1000UL / portTICK_PERIOD_MS;
+long lBytes;
 
+    /* Send strings to port 10000 on IP address 192.168.0.188. */
+    xDestinationAddress.sin_addr = FreeRTOS_inet_addr( "192.168.0.90" );
+    xDestinationAddress.sin_port = FreeRTOS_htons( 10000 );
 
-static const unsigned mainDELAY_LOOP_COUNT = 10000;
+    /* Create the socket. */
+    xSocket = FreeRTOS_socket( FREERTOS_AF_INET,
+                               FREERTOS_SOCK_DGRAM,
+                               FREERTOS_IPPROTO_UDP );
 
-SemaphoreHandle_t sem;
+    /* Check the socket was created. */
+    configASSERT( xSocket != FREERTOS_INVALID_SOCKET );
 
+    /* NOTE: FreeRTOS_bind() is not called.  This will only work if
+    ipconfigALLOW_SOCKET_SEND_WITHOUT_BIND is set to 1 in FreeRTOSIPConfig.h. */
 
-BaseType_t sensor_irq_handler() {    
-    BaseType_t pxHigherPriorityTaskWoken = pdFALSE;
-    BaseType_t ans = xSemaphoreGiveFromISR(sem, &pxHigherPriorityTaskWoken);
-    
-    // Note: use *portRETURN_FROM_ISR* to return to the task that was interrupted, and use *portYIELD_FROM_ISR* to trigger a scheduler context switch (if necessary) in order to immediately return to the woken task
-    
-	//portRETURN_FROM_ISR;
-	portYIELD_FROM_ISR( pxHigherPriorityTaskWoken );
-}
+    for( ;; )
+    {
+        /* Create the string that is sent. */
+        sprintf( cString,
+                 "Standard send message number %lu\r\n",
+                 ulCount );
+                 
+        printf("[freertos] send:\n");
+        printf(cString);
 
-void vTaskSomething(void *pvParameters) {
-    volatile uint32_t ul;
-    for (;;) {
-        printf("Other Task Running @%d\n", xTaskGetTickCount());
-    
-        for (ul = 0; ul < mainDELAY_LOOP_COUNT; ul++)
-	        ;
+        /* Send the string to the socket.  ulFlags is set to 0, so the standard
+        semantics are used.  That means the data from cString[] is copied
+        into a network buffer inside FreeRTOS_sendto(), and cString[] can be
+        reused as soon as FreeRTOS_sendto() has returned. */
+        lBytes = FreeRTOS_sendto( xSocket,
+                                  cString,
+                                  strlen( cString ),
+                                  0,
+                                  &xDestinationAddress,
+                                  sizeof( xDestinationAddress ) );
+                                  
+        if( lBytes > 0 )
+        {
+            /* Data was received and can be process here. */
+            printf("[freertos] successful send (bytes > 0)\n");
+        } else {
+            printf("[freertos] send returned zero bytes\n");
+        }
+
+        ulCount++;
+
+        /* Wait until it is time to send again. */
+        vTaskDelay( x1000ms );
     }
 }
 
-void vTaskDumpSensorData(void *pvParameters) {
-    const TickType_t xDelay100ms = pdMS_TO_TICKS( 100 );
-    for (;;) {
-        BaseType_t ans = xSemaphoreTake(sem, xDelay100ms);
-        if (ans == pdFALSE) {
-            printf("Dump Task sensor data not available @%d\n", xTaskGetTickCount());
-        } else {
-            printf("Dump Task Running @%d\n", xTaskGetTickCount());
-        	for (int i=0; i<64; ++i) {
-		        *TERMINAL_ADDR = *(SENSOR_INPUT_ADDR + i) % 92 + 32;
-	        }
-	        *TERMINAL_ADDR = '\n';
+
+static void vReceivingUsingStandardInterface( void *pvParameters )
+{
+long lBytes;
+uint8_t cReceivedString[ 60 ];
+struct freertos_sockaddr xClient, xBindAddress;
+uint32_t xClientLength = sizeof( xClient );
+xSocket_t xListeningSocket;
+
+    /* Attempt to open the socket. */
+    xListeningSocket = FreeRTOS_socket( FREERTOS_AF_INET,
+                                        FREERTOS_SOCK_DGRAM,
+                                        FREERTOS_IPPROTO_UDP );
+
+    /* Check the socket was created. */
+    configASSERT( xListeningSocket != FREERTOS_INVALID_SOCKET );
+
+    /* Bind to port 10000. */
+    xBindAddress.sin_port = FreeRTOS_htons( 10001 );
+    FreeRTOS_bind( xListeningSocket, &xBindAddress, sizeof( xBindAddress ) );
+
+    for( ;; )
+    {
+        printf("[freertos] begin-recv:\n");
+        
+        memset(cReceivedString, 0, 60);
+    
+        /* Receive data from the socket.  ulFlags is zero, so the standard
+        interface is used.  By default the block time is portMAX_DELAY, but it
+        can be changed using FreeRTOS_setsockopt(). */
+        lBytes = FreeRTOS_recvfrom( xListeningSocket,
+                                    cReceivedString,
+                                    sizeof( cReceivedString ),
+                                    0,
+                                    &xClient,
+                                    &xClientLength );
+                                    
+        printf("[freertos] end-recv:\n");
+
+        if( lBytes > 0 )
+        {
+            /* Data was received and can be process here. */
+            printf("[freertos] recv:\n");
+            printf(cReceivedString);
         }
     }
 }
 
-int main() {
-	register_interrupt_handler(2, sensor_irq_handler);
-	
-	*SENSOR_SCALER_REG_ADDR = 10;
-	*SENSOR_FILTER_REG_ADDR = 2;
-	
-	sem = xSemaphoreCreateBinary();
-	configASSERT( sem != NULL );
-	
-	xTaskCreate( vTaskSomething, "Task 1", 1000, NULL, 1, NULL );
-	xTaskCreate( vTaskDumpSensorData, "Task 2", 1000, NULL, 2, NULL );
 
+/* Define the network addressing.  These parameters will be used if either
+ipconfigUDE_DHCP is 0 or if ipconfigUSE_DHCP is 1 but DHCP auto configuration
+failed. */
+static const uint8_t ucIPAddress[ 4 ] = { 192, 168, 0, 200 };
+static const uint8_t ucNetMask[ 4 ] = { 255, 255, 255, 255 };
+static const uint8_t ucGatewayAddress[ 4 ] = { 192, 168, 0, 1 };
+
+/* The following is the address of an OpenDNS server. */
+static const uint8_t ucDNSServerAddress[ 4 ] = { 208, 67, 222, 222 };
+
+/* The MAC address array is not declared const as the MAC address will
+normally be read from an EEPROM and not hard coded (in real deployed
+applications).*/
+//static uint8_t ucMACAddress[ 6 ] = { 0x54, 0xe1, 0xad, 0x74, 0x33, 0xfd };
+static uint8_t ucMACAddress[ 6 ] = { 0x72, 0x5c, 0xb4, 0x2c, 0xac, 0x4a };
+
+int main( void )
+{
+    /* Initialise the embedded Ethernet interface.  The tasks that use the
+    network are created in the vApplicationIPNetworkEventHook() hook function
+    below.  The hook function is called when the network connects. */
+    FreeRTOS_IPInit( ucIPAddress,
+                     ucNetMask,
+                     ucGatewayAddress,
+                     ucDNSServerAddress,
+                     ucMACAddress );
+
+    /*
+     * Other RTOS tasks can be created here.
+     */
+
+    /* Start the RTOS scheduler. */
     vTaskStartScheduler();
-		
-	configASSERT( 0 );
+
+    /* If all is well, the scheduler will now be running, and the following
+    line will never be reached.  If the following line does execute, then
+    there was insufficient FreeRTOS heap memory available for the idle and/or
+    timer tasks to be created. */
+    exit(0);
 }
+
+
+void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
+{
+    uint32_t ulIPAddress, ulNetMask, ulGatewayAddress, ulDNSServerAddress;
+    static BaseType_t xTasksAlreadyCreated = pdFALSE;
+    int8_t cBuffer[ 16 ];
+
+    /* Check this was a network up event, as opposed to a network down event. */
+    if( eNetworkEvent == eNetworkUp )
+    {
+        /* The network is up and configured.  Print out the configuration,
+        which may have been obtained from a DHCP server. */
+        FreeRTOS_GetAddressConfiguration( &ulIPAddress,
+                                          &ulNetMask,
+                                          &ulGatewayAddress,
+                                          &ulDNSServerAddress );
+                                          
+        printf("[freertos] network up\n");
+
+        /* Convert the IP address to a string then print it out. */
+        FreeRTOS_inet_ntoa( ulIPAddress, cBuffer );
+        printf( "IP Address: %s\r\n", cBuffer );
+
+        /* Convert the net mask to a string then print it out. */
+        FreeRTOS_inet_ntoa( ulNetMask, cBuffer );
+        printf( "Subnet Mask: %s\r\n", cBuffer );
+
+        /* Convert the IP address of the gateway to a string then print it out. */
+        FreeRTOS_inet_ntoa( ulGatewayAddress, cBuffer );
+        printf( "Gateway IP Address: %s\r\n", cBuffer );
+
+        /* Convert the IP address of the DNS server to a string then print it out. */
+        FreeRTOS_inet_ntoa( ulDNSServerAddress, cBuffer );
+        printf( "DNS server IP Address: %s\r\n", cBuffer );
+        
+        /* Create the tasks that use the IP stack if they have not already been
+        created. */
+        if( xTasksAlreadyCreated == pdFALSE )
+        {
+            /*
+             * Create the tasks here.
+             */
+             xTaskCreate( vReceivingUsingStandardInterface, "Task Receive", 1000, NULL, 1, NULL );
+             xTaskCreate( vSendUsingStandardInterface, "Task Send", 1000, NULL, 1, NULL );
+
+            xTasksAlreadyCreated = pdTRUE;
+        }
+    } else if( eNetworkEvent == eNetworkDown ) {
+        printf("[freertos] network down\n");
+    }
+}
+
 /*-----------------------------------------------------------*/
 
 void vApplicationMallocFailedHook( void )
