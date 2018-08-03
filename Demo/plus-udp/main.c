6b733e03e167aea269e1a8db25adc865139e41cc
Pascal Pieper <ppieper@informatik.uni-bremen.de>
2018-08-03 14:58:09 +0200

    Now using broadcast. Somehow program stops after message number 2

--- Demo/plus-udp/main.c
+++ Demo/plus-udp/main.c
@@ -58,124 +58,18 @@ void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent );
 /*-----------------------------------------------------------*/
 
 
-static void vSendUsingStandardInterface( void *pvParameters )
-{
-xSocket_t xSocket;
-struct freertos_sockaddr xDestinationAddress;
-int8_t cString[ 50 ];
-uint32_t ulCount = 0UL;
-const TickType_t x1000ms = 1000UL / portTICK_PERIOD_MS;
-long lBytes;
-
-    xDestinationAddress.sin_addr = FreeRTOS_inet_addr( "134.102.218.255" );
-    xDestinationAddress.sin_port = FreeRTOS_htons( 10000 );
-
-    /* Create the socket. */
-    xSocket = FreeRTOS_socket( FREERTOS_AF_INET,
-                               FREERTOS_SOCK_DGRAM,
-                               FREERTOS_IPPROTO_UDP );
-
-    /* Check the socket was created. */
-    configASSERT( xSocket != FREERTOS_INVALID_SOCKET );
-
-    /* NOTE: FreeRTOS_bind() is not called.  This will only work if
-    ipconfigALLOW_SOCKET_SEND_WITHOUT_BIND is set to 1 in FreeRTOSIPConfig.h. */
-
-    for( ;; )
-    {
-        /* Create the string that is sent. */
-        sprintf( cString,
-                 "Standard send message number %" PRIu32 "\r\n",
-                 ulCount );
-                 
-        printf("[freertos] send:\n");
-        printf(cString);
-
-        /* Send the string to the socket.  ulFlags is set to 0, so the standard
-        semantics are used.  That means the data from cString[] is copied
-        into a network buffer inside FreeRTOS_sendto(), and cString[] can be
-        reused as soon as FreeRTOS_sendto() has returned. */
-        lBytes = FreeRTOS_sendto( xSocket,
-                                  cString,
-                                  strlen( cString ),
-                                  0,
-                                  &xDestinationAddress,
-                                  sizeof( xDestinationAddress ) );
-                                  
-        if( lBytes > 0 )
-        {
-            /* Data was received and can be process here. */
-            printf("[freertos] successful send (bytes > 0)\n");
-        } else {
-            printf("[freertos] send returned zero bytes\n");
-        }
-
-        ulCount++;
-
-        /* Wait until it is time to send again. */
-        vTaskDelay( x1000ms );
-    }
-}
-
-
-static void vReceivingUsingStandardInterface( void *pvParameters )
-{
-long lBytes;
-uint8_t cReceivedString[ 60 ];
-struct freertos_sockaddr xClient, xBindAddress;
-uint32_t xClientLength = sizeof( xClient );
-xSocket_t xListeningSocket;
-
-    /* Attempt to open the socket. */
-    xListeningSocket = FreeRTOS_socket( FREERTOS_AF_INET,
-                                        FREERTOS_SOCK_DGRAM,
-                                        FREERTOS_IPPROTO_UDP );
-
-    /* Check the socket was created. */
-    configASSERT( xListeningSocket != FREERTOS_INVALID_SOCKET );
-
-    /* Bind to port 10000. */
-    xBindAddress.sin_port = FreeRTOS_htons( 10001 );
-    FreeRTOS_bind( xListeningSocket, &xBindAddress, sizeof( xBindAddress ) );
-
-    for( ;; )
-    {
-        printf("[freertos] begin-recv:\n");
-        
-        memset(cReceivedString, 0, 60);
-    
-        /* Receive data from the socket.  ulFlags is zero, so the standard
-        interface is used.  By default the block time is portMAX_DELAY, but it
-        can be changed using FreeRTOS_setsockopt(). */
-        lBytes = FreeRTOS_recvfrom( xListeningSocket,
-                                    cReceivedString,
-                                    sizeof( cReceivedString ),
-                                    0,
-                                    &xClient,
-                                    &xClientLength );
-                                    
-        printf("[freertos] end-recv:\n");
-
-        if( lBytes > 0 )
-        {
-            /* Data was received and can be process here. */
-            printf("[freertos] recv:\n");
-            printf(cReceivedString);
-        }
-    }
-}
+static void vSendUsingStandardInterface( void *pvParameters );
+static void vReceivingUsingStandardInterface( void *pvParameters );
 
 
 /* Define the network addressing.  These parameters will be used if either
 ipconfigUDE_DHCP is 0 or if ipconfigUSE_DHCP is 1 but DHCP auto configuration
 failed. */
-static const uint8_t ucIPAddress[ 4 ] = { 0, 0, 0, 0 };
-static const uint8_t ucNetMask  [ 4 ] = { 255, 255, 255, 0 };
-static const uint8_t ucGatewayAddress[ 4 ] = { 0, 0, 0, 0 };
-
+static uint8_t ucIPAddress[ 4 ] = { 0, 0, 0, 0 };
+static uint8_t ucNetMask  [ 4 ] = { 255, 255, 255, 0 };
+static uint8_t ucGatewayAddress[ 4 ] = { 0, 0, 0, 0 };
 /* The following is the address of an OpenDNS server. */
-static const uint8_t ucDNSServerAddress[ 4 ] = { 208, 67, 222, 222 };
-
+static uint8_t ucDNSServerAddress[ 4 ] = { 208, 67, 222, 222 };
 static uint8_t ucMACAddress[ 6 ];
 
 void getMac(uint8_t mac[6])
@@ -194,6 +88,9 @@ int main( void )
     /* Initialise the embedded Ethernet interface.  The tasks that use the
     network are created in the vApplicationIPNetworkEventHook() hook function
     below.  The hook function is called when the network connects. */
+
+	printf("Starting Network.\n");
+
     FreeRTOS_IPInit( ucIPAddress,
                      ucNetMask,
                      ucGatewayAddress,
@@ -248,6 +145,11 @@ void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
         FreeRTOS_inet_ntoa( ulDNSServerAddress, cBuffer );
         printf( "DNS server IP Address: %s\r\n", cBuffer );
         
+        memcpy(ucIPAddress, &ulIPAddress, 4);
+        memcpy(ucNetMask, &ulNetMask, 4);
+        memcpy(ucGatewayAddress, &ulGatewayAddress, 4);
+        memcpy(ucDNSServerAddress, &ulDNSServerAddress, 4);
+
         /* Create the tasks that use the IP stack if they have not already been
         created. */
         if( xTasksAlreadyCreated == pdFALSE )
@@ -262,6 +164,116 @@ void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
         }
     } else if( eNetworkEvent == eNetworkDown ) {
         printf("[freertos] network down\n");
+    }
+}
+
+static void vSendUsingStandardInterface( void *pvParameters )
+{
+xSocket_t xSocket;
+struct freertos_sockaddr xDestinationAddress;
+int8_t cString[ 50 ];
+uint32_t ulCount = 0UL;
+const TickType_t x1000ms = 1000UL / portTICK_PERIOD_MS;
+long lBytes;
+
+	uint32_t networkBroadcast = (*(uint32_t*)ucIPAddress) | ~(*(uint32_t*)ucNetMask);
+    xDestinationAddress.sin_addr = networkBroadcast;
+    xDestinationAddress.sin_port = FreeRTOS_htons( 10000 );
+
+    /* Create the socket. */
+    xSocket = FreeRTOS_socket( FREERTOS_AF_INET,
+                               FREERTOS_SOCK_DGRAM,
+                               FREERTOS_IPPROTO_UDP );
+
+    /* Check the socket was created. */
+    configASSERT( xSocket != FREERTOS_INVALID_SOCKET );
+
+    /* NOTE: FreeRTOS_bind() is not called.  This will only work if
+    ipconfigALLOW_SOCKET_SEND_WITHOUT_BIND is set to 1 in FreeRTOSIPConfig.h. */
+
+    for( ;; )
+    {
+        /* Create the string that is sent. */
+        sprintf( cString,
+                 "Standard send message number %" PRIu32 "\r\n",
+                 ulCount );
+
+        uint8_t cBuffer[16];
+        FreeRTOS_inet_ntoa(networkBroadcast, cBuffer );
+
+        printf("[freertos] send to %s:\n", cBuffer);
+        printf(cString);
+
+        /* Send the string to the socket.  ulFlags is set to 0, so the standard
+        semantics are used.  That means the data from cString[] is copied
+        into a network buffer inside FreeRTOS_sendto(), and cString[] can be
+        reused as soon as FreeRTOS_sendto() has returned. */
+        lBytes = FreeRTOS_sendto( xSocket,
+                                  cString,
+                                  strlen( cString ),
+                                  0,
+                                  &xDestinationAddress,
+                                  sizeof( xDestinationAddress ) );
+
+        if( lBytes > 0 )
+        {
+            /* Data was received and can be process here. */
+            printf("[freertos] successful send (bytes %d > 0)\n", lBytes);
+        } else {
+            printf("[freertos] send returned zero bytes\n");
+        }
+
+        ulCount++;
+
+        /* Wait until it is time to send again. */
+        vTaskDelay( x1000ms );
+    }
+}
+
+static void vReceivingUsingStandardInterface( void *pvParameters )
+{
+long lBytes;
+uint8_t cReceivedString[ 60 ];
+struct freertos_sockaddr xClient, xBindAddress;
+uint32_t xClientLength = sizeof( xClient );
+xSocket_t xListeningSocket;
+
+    /* Attempt to open the socket. */
+    xListeningSocket = FreeRTOS_socket( FREERTOS_AF_INET,
+                                        FREERTOS_SOCK_DGRAM,
+                                        FREERTOS_IPPROTO_UDP );
+
+    /* Check the socket was created. */
+    configASSERT( xListeningSocket != FREERTOS_INVALID_SOCKET );
+
+    /* Bind to port 10000. */
+    xBindAddress.sin_port = FreeRTOS_htons( 10001 );
+    FreeRTOS_bind( xListeningSocket, &xBindAddress, sizeof( xBindAddress ) );
+
+    for( ;; )
+    {
+        printf("[freertos] begin-recv:\n");
+
+        memset(cReceivedString, 0, 60);
+
+        /* Receive data from the socket.  ulFlags is zero, so the standard
+        interface is used.  By default the block time is portMAX_DELAY, but it
+        can be changed using FreeRTOS_setsockopt(). */
+        lBytes = FreeRTOS_recvfrom( xListeningSocket,
+                                    cReceivedString,
+                                    sizeof( cReceivedString ),
+                                    0,
+                                    &xClient,
+                                    &xClientLength );
+
+        printf("[freertos] end-recv:\n");
+
+        if( lBytes > 0 )
+        {
+            /* Data was received and can be process here. */
+            printf("[freertos] recv:\n");
+            printf(cReceivedString);
+        }
     }
 }
 
